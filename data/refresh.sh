95eadf79bd3e4ce1a0fa29fb7ffa40d1ca7b1d10
michaelcmelton <michaelcmelton93@gmail.com>
2024-11-22 22:52:14 -0500

    feat(data): quest json

--- data/refresh.sh
+++ data/refresh.sh
@@ -1,14 +1,14 @@
-#! /bin/sh
+#! /bin/bash
 
 # Get JSON
-curl https://oldschool.runescape.wiki/api.php\?action\=ask\&format\=json\&query\=%5B%5BQuest%20JSON%3A%3A%2B%5D%5D%7C%3FQuest%20JSON%7Climit%3D10000\&formatversion\=2 > ./quest_json.json
+curl https://oldschool.runescape.wiki/api.php\?action\=ask\&format\=json\&query\=%5B%5BQuest%20JSON%3A%3A%2B%5D%5D%7C%3FQuest%20JSON%7Climit%3D10000\&formatversion\=2 > ./data/quest_json.json
 
 # prevent the printing of the directory with command substitution
-sed -iE 's/\* /*/g' ./quest_json.json
-echo $(cat ./quest_json.json | jq '.query.results') > ./quest_json.json
+sed -iE 's/\* /*/g' ./data/quest_json.json
+echo $(cat ./data/quest_json.json | jq '.query.results') > ./data/quest_json.json
 
 # Replace HTML Enitities with proper characters
-FILE_CONTENTS=$(cat ./quest_json.json | perl -CS -MHTML::Entities -ne 'print decode_entities($_)') 
+FILE_CONTENTS=$(cat ./data/quest_json.json | perl -CS -MHTML::Entities -ne 'print decode_entities($_)') 
 
 # Replace ''' with '
 FILE_CONTENTS=$(echo $FILE_CONTENTS | sed -E 's/\'\'\'/'/g')
@@ -39,8 +39,7 @@ FILE_CONTENTS=$(echo $FILE_CONTENTS | sed -E 's/title="([a-zA-Z 0-9;]+)"/title=\
 FILE_CONTENTS=$(echo $FILE_CONTENTS | sed -E 's/style="([a-zA-Z:; 0-9-]+)"/style=\\"\1\\"/g')
 FILE_CONTENTS=$(echo $FILE_CONTENTS | sed -E "s/\'\"\`UNIQ.+\`\"\'//g")
 FILE_CONTENTS=$(echo $FILE_CONTENTS | sed -E 's/SMW\\Serializers\\QueryResultSerializer/SMW\\\\Serializers\\\\QueryResultSerializer/g')
-echo "$FILE_CONTENTS" > ./updated.json
+echo "$FILE_CONTENTS" > ./data/updated.json
 
-echo $(cat updated.json | jq 'with_entries(.value |= .printouts."Quest JSON")') > ./updated.json
-
-rm ./quest_json.jsonE
+echo $(cat ./data/updated.json | jq 'with_entries(.value |= .printouts."Quest JSON")') > ./data/final_json.json
+rm ./data/quest_json.jsonE

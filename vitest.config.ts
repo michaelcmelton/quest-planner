360ec5ef38395b388638c80fed8ab6cb1841643e
michaelcmelton <michaelcmelton93@gmail.com>
2025-04-05 23:27:08 -0400

    maint: implement coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig(({ mode }) => ({
     plugins: [sveltekit()],
     test: {
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+            exclude: ['node_modules', 'scripts', '.svelte-kit', 'public', '*.config.{ts,js}', 'src/lib/types', '*.d.ts'],
+        },
         include: ['**/**/*.{test,spec}.{js,ts}'],
         environment: 'jsdom',
         globals: true,
